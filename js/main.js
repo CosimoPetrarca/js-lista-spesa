9a3d8071f95be092342ffa362c3e7c76c52cf8e9
P4anix <pancok_1@yahoo.it>
2023-02-28 17:15:35 +0100

    addition HTML

--- js/main.js
+++ js/main.js
@@ -1,13 +1,19 @@
 'Use strict'
 
 /* definiamo una lista di prodotti */
-const lista = ['vino', 'birra', 'superalcolici', 'vodka', 'Campari', 'Amaro Lucano', 'Red Bull'];
+const lista = ['Vino', 'Birra', 'Superalcolici', 'Vodka', 'Campari', 'Amaro Lucano', 'Red Bull'];
+
+/* Selezioniamo l'elemento UL dalla pagina HTML */
+const listaSpesa = document.getElementById('lista-spesa');
 
 /* inizializziamo l'indice a 0 */
 let i = 0;
 
 /* usiamo il ciclo while per stamparci ogni prodotto della lista */
 while (i < lista.length) {
-    console.log (lista[i]);
+    console.log(lista[i]);
+    const listItem = document.createElement('li');
+    listItem.textContent = lista [i];
+    listaSpesa.append(listItem);
     i++;
 }
