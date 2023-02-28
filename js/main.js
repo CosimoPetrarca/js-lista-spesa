110b873332641dacac7b6e450b483fda08a7223a
P4anix <pancok_1@yahoo.it>
2023-02-28 15:47:31 +0100

    Js done

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 'Use strict'
+
+/* definiamo una lista di prodotti */
+const lista = ['vino', 'birra', 'superalcolici', 'vodka', 'Campari', 'Amaro Lucano', 'Red Bull'];
+
+/* inizializziamo l'indice a 0 */
+let i = 0;
+
+/* usiamo il ciclo while per stamparci ogni prodotto della lista */
+while (i < lista.length) {
+    console.log (lista[i]);
+    i++;
+}
